eb98e593d65d68d8763590bcea01052114640551
eric he <ehe@accela.com>
2020-04-06 22:28:45 +0800

    修改Swagger-UI API文档描述

--- web-app/src/main/java/com/cnblogs/kmpp/swagger2/SwaggerConfig.java
+++ web-app/src/main/java/com/cnblogs/kmpp/swagger2/SwaggerConfig.java
@@ -21,11 +21,10 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 /*
-                 重要的两个方法:
-                 apis():指定要生成文档的接口包基本路径
-                 paths():指定针对哪些请求生成接口文档
-                 参考官方资料：http://www.baeldung.com/swagger-2-documentation-for-spring-rest-api
-                 ****/
+                 * 重要的两个方法:
+                 *                  apis():指定要生成文档的接口包基本路径
+                 *                  paths():指定针对哪些请求生成接口文档
+                 */
                 //.apis(RequestHandlerSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.cnblogs.kmpp.api"))
                 .paths(PathSelectors.ant("/api/**"))
@@ -36,7 +35,7 @@ public class SwaggerConfig {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("SpringMVC Multi Module Project Demo")
-                .description("项目主要用于学习")
+                .description("源码主要用于学习整合SSM框架：SpringMVC + Spring + MyBatis 构建maven多模块工程项目，根据不同功能划分模块，方便管理和解耦。项目主要涉及到的内容有：基于SpringMVC框架（非SpringBoot启动方式），结合MyBatis构建数据库访问层，以及使用AOP实现方法切面拦截，以及结合Redis高度封装类RedisTemplate实现数据缓存增、删、改、查操作。")
                 .version("1.0.0")
                 .termsOfServiceUrl("")
                 .license("")
